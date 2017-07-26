b717e1ea0bafc5b21841e1a549da11e740e23ec9
monique berry <moniqueberry88@gmail.com>
2017-07-26 16:48:02 -0500

    search done

--- server/procedures/search.proc.js
+++ server/procedures/search.proc.js
@@ -1,8 +1,14 @@
 const mdb = require('moviedb')(process.env.TMDB_API_KEY);
 
-exports.search = function(movie){
-    return mdb.searchMovie({ query: movie }, (err, res) => {
-    console.log(res);
-    res.send(movie);
-});
-}
+exports.getMovieInfo = function(movie) {
+	return new Promise(function(fulfill, reject) {
+		mdb.searchMovie({ query: movie }, (err, res) => {
+			if(err) {
+				reject(err);
+			} else {
+				fulfill(res);
+			}
+		});
+	});
+};
+
