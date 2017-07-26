a946ee94daab3eefe49efa4f6206c86a1a66fac3
makozlo <makozlo@gmail.com>
2017-07-26 18:58:19 -0500

    front-end additions

--- client/controllers/DetailController.js
+++ client/controllers/DetailController.js
@@ -1,6 +1,8 @@
 app.controller('DetailController', ['$scope', '$http', 'DetailFactory', function($scope, $http, DetailFactory) {
 
 	$scope.movie = DetailFactory.get({id: '315635'});
+	
+	$scope.poster = $scope.movie.poster;
 
 	$scope.getSpiderMan = function() {
 		console.log($scope.movie);
@@ -8,5 +10,13 @@ app.controller('DetailController', ['$scope', '$http', 'DetailFactory', function
 
     $scope.getTitle = function() {
     	console.log($scope.movie.title);
+    	console.log($scope.movie);
+	console.log($scope.movie.title);
+	console.log($scope.movie.homepage);
+	console.log($scope.movie.poster_path);
+    };
+
+    $scope.getPoster = function() {
+    	console.log('https://image.tmdb.org/t/p/w500' + $scope.movie.poster_path);
     };
 }]);
