340d6c7eb8d8d73997a9aa0c477d1c291557071f
makozlo <makozlo@gmail.com>
2017-07-28 15:45:56 -0500

    worked on dashboard

--- client/controllers/DashboardController.js
+++ client/controllers/DashboardController.js
@@ -1,3 +1,11 @@
-app.controller('DashboardController', ['$scope', '$http', function ($scope, $http) {
+app.controller('DashboardController', ['$scope', '$http', 'DashboardFactory', function ($scope, $http, DashboardFactory) {
+	userid = 2;
+	var data = new DashboardFactory({id: userid});
+
+	data.$get(function(data) {
+		$scope.otherLists = data.otherLists;
+		// console.log($scope.lists);
+		// console.log($scope.lists.otherLists);
+	});
 
 }]);
