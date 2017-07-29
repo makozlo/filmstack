6eece46035e6314373e62591ae2ee08d10554cf2
monique berry <moniqueberry88@gmail.com>
2017-07-29 17:03:14 -0500

    this should work

--- client/controllers/SplashController.js
+++ client/controllers/SplashController.js
@@ -12,33 +12,8 @@ app.controller('SplashController', ['$scope', '$resource', '$http', 'SearchFacto
 			$location.path('/search-results');
 		});
 
-<<<<<<< HEAD
-		
-		// $scope.results = SearchFactory.save({query: $scope.query});
-		// console.log($scope.results);
-		// $http.post('http://localhost:3000/api/search', { keyword: $scope.query })
-		// 	.then(function (data) {
-		// 		$scope.results = data.data.results[0];
-
-		// 		$scope.firstResultTitle = data.data.results[0].title;
-		// 		console.log($scope.results);
-		// 		console.log($scope.firstResultTitle);
-		// 		console.log(data.data);
-		// 	}, function () {
-		// 		console.log('nope');
-		// 	});
-		// .then(function(data) {
-		// 	$location.path('/search-results');
-		// });
-
-	};
-
-	$scope.createUser = function () {
-			var user = new User({
-=======
 		$scope.createUser = function () {
 			var user = new UserFactory({
->>>>>>> 996d22bf739eb6ae9aaee573e25e95af4200538d
 				username: $scope.username,
 				email: $scope.email,
 				password: $scope.password
