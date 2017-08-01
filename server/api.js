8555d4163a4f9832b144817a168ea249947161f8
Paul Dobbs <pldobbs@mac.com>
2017-07-31 19:18:41 -0500

    api.js lists route added, lists.ctrl.js added and lists.proc.js added

--- server/api.js
+++ server/api.js
@@ -4,7 +4,9 @@ var search = require('./controllers/search.ctrl');
 var dashboard = require('./controllers/dashboard.ctrl');
 var user = require('./controllers/users.ctrl');
 var castCrew = require('./controllers/castCrew.ctrl');
+var lists = require('./controllers/lists.ctrl');
 var router = express.Router();
+
 
 //AT THIS POINT THE URL IS HTTP://LOCALHOST:3000/API    
 router.use('/detail', detail);
@@ -12,5 +14,6 @@ router.use('/search', search);
 router.use('/dashboard', dashboard);
 router.use('/users', user);
 router.use('/cast-crew', castCrew);
+router.use('/lists/:id', lists)
 
 module.exports = router;
