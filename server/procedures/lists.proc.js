519235099686342a94a6622f61321bd50535782c
monique berry <moniqueberry88@gmail.com>
2017-08-03 11:52:12 -0500

    parameter change

--- server/procedures/lists.proc.js
+++ server/procedures/lists.proc.js
@@ -7,8 +7,8 @@ exports.addMovie = function(id, title, poster, listID){
 //     return db.empty('add_to_movie_lists', [movieID, listID]);
 // };
 
-exports.addToSpecificList = function(movieID, title, poster_path, listID, listName) {
-    return db.empty('add_movie_to_specific_list', [movieID, title, poster_path, listID, listName]);
+exports.addToSpecificList = function(movieID, title, poster_path, listID, userID) {
+    return db.empty('add_movie_to_specific_list', [movieID, title, poster_path, listID, userID]);
 }
 exports.all = function(id) {
     return db.rows('get_all_user_lists', [id]);
